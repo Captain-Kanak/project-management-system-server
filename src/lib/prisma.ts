4f41644eb95db22a9fdad0f12c79be0dd085ff4e
Captain-Kanak <kanakroy835@gmail.com>
2026-01-21 18:59:01 +0600

    added: make ERD and define schema for db

---
+++ src/lib/prisma.ts
@@ -0,0 +1,10 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/prisma/client";
+import { envConfig } from "../config/envConfig";
+
+const connectionString = `${envConfig.datebase_url}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import { app } from "./app";
 import { envConfig } from "./config/envConfig";
+import { prisma } from "./lib/prisma";
 
 const port = envConfig.port || 5000;
 
 async function Server() {
   try {
+    await prisma.$connect();
+    console.log("Database Connected Successfully using Prisma.");
+
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${port}`);
     });
